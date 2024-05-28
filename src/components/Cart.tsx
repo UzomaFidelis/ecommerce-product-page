e5e2b95204aa268ad1d5f3cedf0c0c9530320021
Uzoma Fidelis <fiddycena@gmail.com>
2024-05-28 23:46:51 +0100

    added cart checkout button

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -15,43 +15,46 @@ const Cart = ({
       <hr className="cart__divider" />
       <div className="cart__content">
         {items.length !== 0 ? (
-          <ul className="cart__items">
-            {items.map((item, idx) => (
-              <li key={idx} className="cart__item">
-                <div className="cart__image-container">
-                  <img
-                    src={item.image}
-                    alt=""
-                    className="cart__image"
-                    aria-hidden
-                  />
-                </div>
-                <div className="cart__item-info">
-                  <p className="cart__item-name">{item.name}</p>
-                  <p className="cart__item-price">
-                    ${item.price.toFixed(2)} x {item.units}{" "}
-                    <span className="cart__item_price cart__item-price_total">
-                      ${(item.price * item.units).toFixed(2)}
-                    </span>
-                  </p>
-                </div>
-                <div className="cart__delete">
-                  <button
-                    aria-label="remove from cart"
-                    className="cart__delete-btn"
-                    onClick={() => onRemoveCartItem(item.productId)}
-                  >
+          <>
+            <ul className="cart__items">
+              {items.map((item, idx) => (
+                <li key={idx} className="cart__item">
+                  <div className="cart__image-container">
                     <img
-                      src={deleteIcon}
+                      src={item.image}
                       alt=""
+                      className="cart__image"
                       aria-hidden
-                      className="cart__delete-btn-image"
                     />
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
+                  </div>
+                  <div className="cart__item-info">
+                    <p className="cart__item-name">{item.name}</p>
+                    <p className="cart__item-price">
+                      ${item.price.toFixed(2)} x {item.units}{" "}
+                      <span className="cart__item_price cart__item-price_total">
+                        ${(item.price * item.units).toFixed(2)}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="cart__delete">
+                    <button
+                      aria-label="remove from cart"
+                      className="cart__delete-btn"
+                      onClick={() => onRemoveCartItem(item.productId)}
+                    >
+                      <img
+                        src={deleteIcon}
+                        alt=""
+                        aria-hidden
+                        className="cart__delete-btn-image"
+                      />
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <button className="cart__checkout-btn">Checkout</button>
+          </>
         ) : (
           <p className="cart__empty">Your cart is empty</p>
         )}
